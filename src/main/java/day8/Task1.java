d2810b329b1fa9fb84c5f9a64074f84c6a5fb106
Dimson85 <9269085062@bk.ru>
2021-10-24 09:18:50 +0300

    add days 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,31 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        String numbers = "";
+        int i = 1;
+        int k = 1;
+        long startTime = System.currentTimeMillis();
+        while (i <= 20000) {
+            numbers = numbers + i + " ";
+            i++;
+        }
+        long stopTime = System.currentTimeMillis();
+        System.out.println(numbers);
+        System.out.println(stopTime - startTime);
+        System.out.println(" ");
+
+        long startTime2 = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder();
+        while (k <= 20000) {
+            sb.append(k).append(" ");
+            k++;
+        }
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println(sb.toString());
+        System.out.println(stopTime2 - startTime2);
+
+
     }
+
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Boieng", 2000, 3000, 546);
+        System.out.println(airplane);
 
     }
 }
